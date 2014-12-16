fd310010011e64346528377beb7017db39e0293b
Nakaya Funada <funafuna38@gmail.com>
2014-12-16 17:42:42 +0900

    Update jquery.simpleMarquee.js

--- jquery.simpleMarquee.js
+++ jquery.simpleMarquee.js
@@ -1,22 +1,9 @@
 /*
  * Simple Marquee jQuery Plug-in
  *
- * Copyright 2012 Seesaa, Inc. (http://www.seesaa.co.jp/) 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
-* http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Date: 2012-09-12
- * Rev:  1.0.00
+ * Copyright 2014 Seesaa, Inc. (http://www.seesaa.co.jp/) 
+ * Released under the MIT license
+ * http://opensource.org/licenses/mit-license.php
  */
 
 $.fn.simpleMarquee = function ( opts ){
